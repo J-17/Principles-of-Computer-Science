30d7b17766ef19c6b80a1432429c6b009aee1a59
J-17 <48809098+J-17@users.noreply.github.com>
2019-11-23 21:57:59 +0800

    Update Student.java

--- Assignment2/src/assignment2/Student.java
+++ Assignment2/src/assignment2/Student.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assignment2;
 import java.util.Scanner;
 
 /**
- * Title: ICT 167 Assignment 2
- * @author Sin Loo MAH (33220079)
+ * Title: Principles of Computer Science Assignment 2
+ * @author Jocelyn MAH 
  * File name: Student
  * Date of submission: 26th October 2018
  * Purpose: This is the based class that contain methods that getting information 
